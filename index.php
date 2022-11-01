4fd5ef2d5c2abede8d9cd1bb950fd85aec3d4118
undangan-trk <117141862+undangan-trk@users.noreply.github.com>
2022-11-01 11:58:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Pernikahan &mdash; Ahmad Zaid & Laila</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Pernikahan Ahmad Zaid & Laila" />
+	<meta name="author" content="Djazuli IT" />
+
+
+	<meta property="og:title" content="Pernikahan Ahmad Zaid & Laila"/>
+	<meta property="og:image" content="images/nikah.png"/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content="Pernikahan Ahmad Zaid & Laila"/>
+	<meta property="og:description" content="Pernikahan Ahmad Zaid & Laila"/>
+	<meta name="twitter:title" content="Pernikahan Ahmad Zaid & Laila" />
+	<meta name="twitter:image" content="images/nikah.png" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
+	<link rel="icon" type="image/png" href="images/diamond.png">
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="js/leaflet/leaflet.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	<style type="text/css">
+		nav{
+			position: fixed;
+		}
+	</style>
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2">
+					<div id="fh5co-logo"><a href="index.html">Pernikahan<strong>.</strong></a></div>
+				</div>
+				<div class="col-xs-10 text-right menu-1">
+					<ul>
+						<li class="active"><a href="#beranda">Beranda</a></li>
+						<li><a href="#acara">Acara</a></li>
+						<li><a href="#map">Denah</a></li>
+						<li><a href="#keutamaan">Keutamaan</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_2.jpg); background-size: cover;" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container" id="beranda">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Zaid &amp; Laila</h1>
+							<h2>Kami Akan Menikah</h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<p><a href="#" class="btn btn-default btn-sm">Mohon Doa Restu</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-couple">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Assalamualaikum!</h2>
+					<h3>Ahad, 3 Juli 2022 Maduran, Lamongan</h3>
+					<p>Akad Nikah Kami</p>
+				</div>
+			</div>
+			<div class="couple-wrap animate-box">
+				<div class="couple-half">
+					<div class="groom">
+						<img src="images/1.png" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-groom">
+						<h3>Ahmad Zaid., S.Pd.</h3>
+						<p>Putra dari (Alm) Bapak Karjimin & Ibu Sukayah</p>
+					</div>
+				</div>
+				<p class="heart text-center"><i class="icon-heart2"></i></p>
+				<div class="couple-half">
+					<div class="bride">
+						<img src="images/2.png" alt="groom" class="img-responsive">
+					</div>
+					<div class="desc-bride">
+						<h3>Laila  Nailatul Yusro, S.Pd.</h3>
+						<p>Putri dari Bapak H.Moh. Yusuf & Ibu Hj. Lilis Sudarwati</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-event" class="fh5co-bg" style="background-image:url(images/img_bg_9.jpg);">
+		<div class="overlay"></div>
+		<div class="container" id="acara">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<span>Acara Spesial Kami</span>
+					<h2>Acara Pernikahan</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="display-t">
+					<div class="display-tc">
+						<div class="col-md-10 col-md-offset-1">
+							<div class="col-md-6 col-sm-6 text-center">
+								<div class="event-wrap animate-box">
+									<h3>AKAD NIKAH</h3>
+									<div class="event-col">
+										<i class="icon-clock"></i>
+										<span>07:00 Pagi</span>
+										<span>10:00 Pagi</span>
+									</div>
+									<div class="event-col">
+										<i class="icon-calendar"></i>
+										<span>Ahad 03</span>
+										<span>Juli, 2022</span>
+									</div>
+									<p>Bertempat di Masjid Jami' Baitur Rahim Desa Pangean Kec. Maduran Kab. Lamongan</p>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6 text-center">
+								<div class="event-wrap animate-box">
+									<h3>RESEPSI</h3>
+									<div class="event-col">
+										<i class="icon-clock"></i>
+										<span>09:00 s/d 11:00</span>
+										<span>13:00 s/d 15:00</span>
+									</div>
+									<div class="event-col">
+										<i class="icon-calendar"></i>
+										<span>Ahad, 3</span>
+										<span>Juli, 2022</span>
+									</div>
+									<p>Kediaman Mempelai Wanita Jl. Bengawan Solo RT/RW 06/02 Desa Pangean Kec. Maduran</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-couple-story">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
+					<h2>Denah Resepsi</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div id='maps-view-absen' style='width: 100%; height:500px;'></div>
+			</div>
+		</div>
+	</div>
+	<div id="fh5co-testimonial">
+		<div class="container" id="keutamaan">
+			<div class="row">
+				<div class="row animate-box">
+					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+						<span>Dalil Pernikahan</span>
+						<h2>Keutamaan Menikah</h2>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12 animate-box">
+						<div class="wrap-testimony">
+							<div class="owl-carousel-fullwidth">
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/nikah.png" alt="user">
+										</figure>
+										<span>Allah Berfirman:</span>
+										<blockquote>
+											<p>“Dan segala sesuatu Kami Ciptakan Berpasang-pasangan supaya kamu mengingat kebesaran Allah.”(QS Az-Zariyat: 49)</p>
+										</blockquote>
+									</div>
+								</div>
+
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/nikah.png" alt="user">
+										</figure>
+										<span>Allah Berfirman:</span>
+										<blockquote>
+											<p>“Bagaimana kamu akan mengambilnya kembali, padahal sebagian kamu telah bergaul (bercampur) dengan yang lain sebagai suami-isteri. Dan mereka (isteri-isterimu) telah mengambil dari kamu perjanjian yang kuat.” (QS An-Nisa: 21)</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/nikah.png" alt="user">
+										</figure>
+										<span>Rasulullah SAW bersabda:</span>
+										<blockquote>
+											<p>“Siapa yang ingin bertemu Allah dalam keadaan suci dan disucikan, maka menikahlah dengan perempuan-perempuan merdeka.” (HR Ibnu Majah)</p>
+										</blockquote>
+									</div>
+								</div>
+								<div class="item">
+									<div class="testimony-slide active text-center">
+										<figure>
+											<img src="images/nikah.png" alt="user">
+										</figure>
+										<span>Rasulullah SAW bersabda:</span>
+										<blockquote>
+											<p> “Siapa yang menikah maka sungguh ia telah diberi setengahnya ibadah.” (HR Abu Ya’la)</p>
+										</blockquote>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2022 <a href="djazuli.my.id">Djazuli Design</a>.</small> 
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+	<audio autoplay="true" loop="true" controls>
+		<source src="audio.mp3" type="audio/mpeg">
+	</audio>
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+
+	<!-- // <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.js"></script> -->
+	<script src="js/simplyCountdown.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<script src="js/leaflet/leaflet.js"></script>
+	<script>
+	    if (document.getElementById("maps-view-absen")) {
+	        var map = L.map('maps-view-absen').setView([-7.014095473315701, 112.27404963692445], 15);
+
+	        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Djazuli Ahmad'
+	        }).addTo(map);
+
+	        L.marker([-7.014095473315701, 112.27404963692445]).addTo(map);
+	    }
+	</script>
+	<script>
+    var d = new Date(new Date().getTime() + 200 * 120 * 120 * 700);
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+</script>
+
+	</body>
+</html>
+
